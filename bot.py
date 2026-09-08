664b19af8bec843edbafaf8086e494f6611a4f0a
KOLPulse <paulbenda81@gmail.com>
2026-09-08 18:54:29 +0200

    Fix PyTgCalls import

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ import os
 from dotenv import load_dotenv
 from fastapi import FastAPI
 from pyrogram import Client, filters
-from pytgcalls import GroupCallFactory
+from pytgcalls import PyTgCalls
 
 load_dotenv()
 
